4cc382ed6d3b68747892ebc4183f4475cd3d92d7
Avxiry <145712927+Avxiry@users.noreply.github.com>
2023-11-27 20:18:21 -0500

    Add files via upload

---
+++ fill_database.py
@@ -0,0 +1,14 @@
+import mysql.connector as mc
+conn = mc.connect(host='localhost', user='root', password='AntLo2552!', db='menagerie')
+c = conn.cursor()
+
+def display_data():
+    c.execute('SELECT name, birth, MONTH(birth) FROM pet')
+    databases = c.fetchall()
+    for database in databases:
+        print(database)
+
+def main():
+    display_data()
+
+main()
